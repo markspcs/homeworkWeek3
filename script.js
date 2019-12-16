945c0518e6835448cea3f6a43f093d42d6794331
markspcs <you@example.com>
2019-12-15 22:31:12 -0600

    finished clipboard

--- script.js
+++ script.js
@@ -7,40 +7,48 @@ const numArr = [...'0123456789'];
 var inputObj = {
   passLength: '10',
   asciiLower: {
-    length: '2',
+    length: 0,
     chars: asciiLowerArr
   },
   asciiUpper: {
-    length: '1',
+    length: 0,
     chars: asciiUpperArr
   },
   specialArg: {
-    length: '2',
+    length: 0,
     chars: specialArr
   },
   numArg: {
-    length: '5',
+    length: 0,
     chars: numArr
   }
 }
 
 var generateBtn = document.querySelector("#generate");
-do {
-  prompt("what's the length?");
-  // prompt("how many ascii characters?");
-  // prompt("how many numbers?");
-  // prompt("how many special characters?");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  // do {
+    inputObj.passLength = prompt("what's the length?");
+    inputObj.asciiLower.length = prompt("how many ascii lower case characters?");
+    inputObj.asciiUpper.length = prompt("how many ascii upper case characters");
+    inputObj.numArg.length = prompt("how many numbers?");
+    inputObj.specialArg.length = prompt("how many special characters?");
 
-} while (validateLength(inputObj));
-delete inputObj.passLength;
-listOfChars = getChars(inputObj);
-randomChars = randomize(listOfChars);
-// test code ////////////////////////////////
+  // } while (validateLength(inputObj));
+  if (validateLength(inputObj)) {
+    delete inputObj.passLength;
+    listOfChars = getChars(inputObj);
+    randomChars = randomize(listOfChars);
+  } else {
+    return "You must ask for a combination of characters between 8 and 128 characters in length";
+  }
+  // test code ////////////////////////////////
 
-console.log("continued code");
-console.log('here');
-for (var i = 0; i < numArr.length; i++) {
-  console.log("char: " + numArr[i]);
+  console.log("continued code");
+  console.log('here');
+  console.log("random" + randomChars);
+  return randomChars;
 }
 ////////////////////////////////////////////////////////
 // Write password to the #password input
@@ -50,23 +58,28 @@ function writePassword() {
 
   passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+  var copyBtn = document.querySelector("#copyToClip");
+   copyBtn.removeAttribute("disabled");
+   //copyBtn.focus("password");
 }
 
 function copyToClipboard() {
+  console.log("clip it");
+  var passText = document.getElementById("password");
+  passText.select();
+  passText.setSelectionRange(0,999);
+  document.execCommand("copy");
+  alert("copied text " + passText.value);
   // BONUS 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // BONUS EVENT LISTENER
 
 /////////////////////////
 // randomizes the list/array that is passed into it. Only external list no objects.
 ////////////////////////////////////////////////
-function randomize(chars){
+function randomize(chars) {
   let randomArr = [];
   for (let i = chars.length; i > 0; i--) {
     let rIndex = Math.floor(Math.random() * chars.length);
@@ -75,6 +88,8 @@ function randomize(chars){
     chars.splice(rIndex, 1);
   }
   console.log(randomArr);
+  var randomString = randomArr.join("");
+  return randomString;
 }
 //////////////////////////////////////////
 //interates through the object that is passed. All objects 
@@ -102,10 +117,15 @@ function validateLength(inputObj) {
   //console.log(inputObj.asciiLength + inputObj.specialArg + inputObj.numArg);
   let overAllLen = Number(inputObj.asciiLower.length) + Number(inputObj.asciiUpper.length) + Number(inputObj.specialArg.length) + Number(inputObj.numArg.length);
   console.log(overAllLen);
-  if (overAllLen === Number(inputObj.passLength)) {
-    return 0;
+  if (inputObj.passLength > 7 && inputObj.passLength < 129) {
+    if (overAllLen === Number(inputObj.passLength)) {
+      return 1;
+    } else {
+      alert("please make sure your number of ascii, number, and special characters add up to the desired over all length\n PLEASE TRY AGAIN!");
+      return 0;
+    }
   } else {
-    alert("please make sure your number of ascii, number, and special characters add up to the desired over all length\n PLEASE TRY AGAIN!");
-    return 1;
+    alert(`you request ${inputObj.passLength} ! The required length is between 8 and 128 characters!`);
+    return 0;
   }
 }
